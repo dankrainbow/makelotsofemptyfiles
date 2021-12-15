6c67472a8a0d27349d671d6835b329a2093e0ae0
dankrainbow <79637386+dankrainbow@users.noreply.github.com>
2021-12-15 20:10:00 +0000

    Update fixlol.py

--- fixlol.py
+++ fixlol.py
@@ -1,4 +1,4 @@
 import os
-for i in range(10): ## change number in brackets to make more files
+for i in range(10): ## change number in brackets to same as the create script if you changed it
 	os.system(f"rm -rf {i}")
 
